ec0538b2e4ed28a1d373ff8bd155b21a24fcba3f
phoeshow.me <phoeshow.me@gmail.com>
2024-11-28 11:00:41 +0800

    remove blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,112 +1,111 @@
-return {}
--- return {
---   "hrsh7th/nvim-cmp",
---   event = "InsertEnter",
---   dependencies = {
---     {
---       "L3MON4D3/LuaSnip",
---       build = "make install_jsregexp",
---       dependencies = {
---         {
---           "rafamadriz/friendly-snippets",
---           config = function()
---             require("luasnip.loaders.from_vscode").lazy_load()
---           end,
---         },
---       },
---     },
---
---     "saadparwaiz1/cmp_luasnip",
---     -- Adds other completion capabilities.
---     --  nvim-cmp does not ship with all sources by default. They are split
---     --  into multiple repos for maintenance purposes.
---     "hrsh7th/cmp-nvim-lsp",
---     "hrsh7th/cmp-path",
---     "hrsh7th/cmp-buffer",
---     "hrsh7th/cmp-cmdline",
---
---     -- add pictograms to cmp-menu
---     "onsails/lspkind.nvim",
---   },
---   config = function()
---     local cmp = require("cmp")
---     local luasnip = require("luasnip")
---     luasnip.config.setup({})
---     local lspkind = require("lspkind")
---
---     cmp.setup({
---       snippet = {
---         expand = function(args)
---           luasnip.lsp_expand(args.body)
---         end,
---       },
---       completion = {
---         completeopt = "menu,menuone,noinsert",
---       },
---       mapping = cmp.mapping.preset.insert({
---         -- Select the [n]ext item
---         ["<C-n>"] = cmp.mapping.select_next_item(),
---         -- Select the [p]revious item
---         ["<C-p>"] = cmp.mapping.select_prev_item(),
---         -- Scroll the documentation window [b]ack / [f]orward
---         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
---         ["<C-f>"] = cmp.mapping.scroll_docs(4),
---         ["<CR>"] = cmp.mapping.confirm({ select = true }),
---         ["<Tab>"] = cmp.mapping.select_next_item(),
---         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
---         -- <c-l> will move you to the right of each of the expansion locations.
---         -- <c-h> is similar, except moving you backwards.
---         ["<C-l>"] = cmp.mapping(function()
---           if luasnip.expand_or_locally_jumpable() then
---             luasnip.expand_or_jump()
---           end
---         end, { "i", "s" }),
---         ["<C-h>"] = cmp.mapping(function()
---           if luasnip.locally_jumpable(-1) then
---             luasnip.jump(-1)
---           end
---         end, { "i", "s" }),
---       }),
---       sources = cmp.config.sources({
---         { name = "lazydev" },
---       }, {
---         { name = "nvim_lsp" },
---         { name = "luasnip" },
---         { name = "path" },
---       }, {
---         { name = "buffer" },
---       }),
---       formatting = {
---         format = lspkind.cmp_format({
---           mode = "symbol_text",
---           menu = {
---             buffer = "[Buffer]",
---             nvim_lsp = "[LSP]",
---             luasnip = "[LuaSnip]",
---             nvim_lua = "[Lua]",
---             latex_symbols = "[Latex]",
---           },
---         }),
---       },
---     })
---
---     -- `/` cmdline setup.
---     cmp.setup.cmdline("/", {
---       mapping = cmp.mapping.preset.cmdline(),
---       sources = {
---         { name = "buffer" },
---       },
---     })
---
---     -- `:` cmdline setup.
---     cmp.setup.cmdline(":", {
---       mapping = cmp.mapping.preset.cmdline(),
---       sources = cmp.config.sources({
---         { name = "path" },
---       }, {
---         { name = "cmdline" },
---       }),
---       matching = { disallow_symbol_nonprefix_matching = false },
---     })
---   end,
--- }
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp",
+      dependencies = {
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+      },
+    },
+
+    "saadparwaiz1/cmp_luasnip",
+    -- Adds other completion capabilities.
+    --  nvim-cmp does not ship with all sources by default. They are split
+    --  into multiple repos for maintenance purposes.
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+
+    -- add pictograms to cmp-menu
+    "onsails/lspkind.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    luasnip.config.setup({})
+    local lspkind = require("lspkind")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      mapping = cmp.mapping.preset.insert({
+        -- Select the [n]ext item
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        -- Select the [p]revious item
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        -- Scroll the documentation window [b]ack / [f]orward
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ["<C-l>"] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { "i", "s" }),
+        ["<C-h>"] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "lazydev" },
+      }, {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          menu = {
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            nvim_lua = "[Lua]",
+            latex_symbols = "[Latex]",
+          },
+        }),
+      },
+    })
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false },
+    })
+  end,
+}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,10 +7,7 @@ return {
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-      },
-      signature = {
-        enabled = false,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
       },
     },
     -- you can enable a preset for easier configuration
