e1b1c29585e18f96fd520f67a894cff6d4980aff
phoeshow.me <phoeshow.me@gmail.com>
2025-03-08 01:58:24 +0800

    update

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -19,11 +19,6 @@ return {
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
     },
-    cmdline = {
-      keymap = {
-        preset = "default",
-      },
-    },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
